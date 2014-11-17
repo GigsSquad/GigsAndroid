b167fb7696b66b88bcf12f5638a8f8ed3fdad69d
evelan <evelan3094@gmail.com>
2014-11-17 16:14:11 +0100

    Polaczenie jsoup z aplikacja w androidzie

--- src/pl/javaparty/concertfinder/MainActivity.java
+++ src/pl/javaparty/concertfinder/MainActivity.java
@@ -1,34 +1,70 @@
 package pl.javaparty.concertfinder;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+import pl.javaparty.jsoup.Concert;
+import pl.javaparty.jsoup.JsoupDownloader;
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+	public static ArrayList<Concert> concerts;
+	Button downloadData;
+	JsoupDownloader jsoupDownloader;
+	StringBuilder stringBuilder;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_main); // ustawiamy layout z "res > layouot > activity_main.xml"
+		downloadData = (Button) findViewById(R.id.downloadButton); // przypisujemy do downloadData przycisk który jest w layoucie "res > layouot > activity_main.xml"
+
+		jsoupDownloader = new JsoupDownloader();
+		stringBuilder = new StringBuilder();
+		concerts = new ArrayList<Concert>();
+
+		downloadData.setOnClickListener(new OnClickListener() { 
+
+			@Override
+			public void onClick(View v) { // to zostanie wykonane po klikniêciu w przycisk "Pobierz" na ekranie 
+				new DownloadTask().execute(); // towrzymy i uruchamiamy obiekt który w tle pobierze dane 
+
+			}
+		});
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+	private class DownloadTask extends AsyncTask<Void, Void, String> {
+		//TODO: zrobiæ informacje ze stanem pobierania
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+		@Override
+		protected String doInBackground(Void... params) { 
+
+			try {
+				jsoupDownloader.getData(); // pobieramy dane 
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			for (Concert c : concerts)
+				stringBuilder.append(c.toString()); //dodajemy informacjê o ka¿dym koncercie do stringa 
+
+			return null;
 		}
-		return super.onOptionsItemSelected(item);
+
+		@Override 
+		protected void onPostExecute(String result) { // zostanie wykonane po skoñczeniu doInBackground
+			super.onPostExecute(result);
+			((TextView) findViewById(R.id.downloadText)).setText(stringBuilder.toString()); //zamiast Hello World ustawiamy stringa z koncertami
+			Toast.makeText(getApplicationContext(), "Pobrano!", Toast.LENGTH_LONG).show(); // wyœwietlanie powiadomienia 
+		}
 	}
+
 }
