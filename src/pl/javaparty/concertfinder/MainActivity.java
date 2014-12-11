f27aa84883dcab630168b8d6fed87fb908715040
evelan <evelan3094@gmail.com>
2014-12-12 00:41:25 +0100

    NavDrawer z przyciskiem, mapy

--- src/pl/javaparty/concertfinder/MainActivity.java
+++ src/pl/javaparty/concertfinder/MainActivity.java
@@ -11,10 +11,12 @@ import android.app.FragmentManager;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
@@ -32,7 +34,7 @@ public class MainActivity extends Activity {
 	int currentFragment = 1;
 	dbManager dbMgr;
 	ConcertManager concertMgr;
-	
+	private ActionBarDrawerToggle mDrawerToggle;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -43,10 +45,10 @@ public class MainActivity extends Activity {
 		concertMgr = new ConcertManager(dbMgr);
 		concertMgr.collect();
 
-		//File db = getDatabasePath(dbManager.DATABASE_NAME);
-		Log.i("DB", "Sprawdzam czy baza istnieje");
-		// if (!db.exists()) TODO: nie zawsze dziala
 		new DownloadTask().execute();
+
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+		getActionBar().setHomeButtonEnabled(true);
 
 		menu = new String[] { "Szukaj", "Ostatnie koncerty", "Twoje koncerty", "Aktualizuj", "Preferencje", "Informacje" };
 		drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -62,7 +64,7 @@ public class MainActivity extends Activity {
 				drawerLayout.closeDrawers();
 
 				if (currentFragment != position) {
-					Fragment fragment = null; 
+					Fragment fragment = null;
 					if (position == 0)
 						fragment = new SearchFragment();
 					else if (position == 1)
@@ -87,6 +89,25 @@ public class MainActivity extends Activity {
 			}
 		});
 
+		mDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout,
+				R.drawable.ic_drawer, // nav menu toggle icon
+				R.string.app_name, // nav drawer open - description for accessibility
+				R.string.app_name // nav drawer close - description for accessibility
+		) {
+			public void onDrawerClosed(View view) {
+				getActionBar().setTitle("dpa");
+				// calling onPrepareOptionsMenu() to show action bar icons
+				invalidateOptionsMenu();
+			}
+
+			public void onDrawerOpened(View drawerView) {
+				getActionBar().setTitle("dupa");
+				// calling onPrepareOptionsMenu() to hide action bar icons
+				invalidateOptionsMenu();
+			}
+		};
+
+		// drawerLayout.openDrawer(new View(getApplicationContext()));
 		FragmentManager fragmentManager = getFragmentManager();
 		Fragment fragment = new RecentFragment();
 		fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
@@ -97,6 +118,21 @@ public class MainActivity extends Activity {
 		MenuInflater inflater = getMenuInflater();
 		inflater.inflate(R.menu.activity_main_actions, menu);
 		return super.onCreateOptionsMenu(menu);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// toggle nav drawer on selecting action bar app icon/title
+		if (mDrawerToggle.onOptionsItemSelected(item)) {
+			return true;
+		}
+		// Handle action bar actions click
+		switch (item.getItemId()) {
+		case 0:
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
 	}
 
 	private class DownloadTask extends AsyncTask<Void, Void, String> {
@@ -118,13 +154,6 @@ public class MainActivity extends Activity {
 			}
 			return null;
 		}
-//
-//		public boolean isOnline() {
-//			ConnectivityManager cm =
-//					(ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-//			NetworkInfo netInfo = cm.getActiveNetworkInfo();
-//			return netInfo != null && netInfo.isConnectedOrConnecting();
-//		}
 
 		@Override
 		protected void onPreExecute() {
