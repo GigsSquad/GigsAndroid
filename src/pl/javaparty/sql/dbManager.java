5d2faa2ce24b8c73e68e095d798a5bedf9de59bf
Amaneusz <kuba.amanowicz@gmail.com>
2014-12-11 20:45:13 +0100

    fuckCM

--- src/pl/javaparty/sql/dbManager.java
+++ src/pl/javaparty/sql/dbManager.java
@@ -1,6 +1,8 @@
 package pl.javaparty.sql;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 
 import android.content.ContentValues;
 import android.content.Context;
@@ -11,6 +13,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class dbManager extends SQLiteOpenHelper {
 
 	public final static String DATABASE_NAME = "baza.db";
+	SQLiteDatabase database;
 
 	private static String CreateConcertTable =
 			"CREATE TABLE Concerts(" +
@@ -32,6 +35,11 @@ public class dbManager extends SQLiteOpenHelper {
 
 	public dbManager(Context context) {
 		super(context, DATABASE_NAME, null, 1);
+		database = getWritableDatabase();
+	}
+	
+	public void close(){
+		database.close();
 	}
 
 	@Override
@@ -45,26 +53,43 @@ public class dbManager extends SQLiteOpenHelper {
 		// TODO Auto-generated method stub
 	}
 
-	public void addConcert(String artistName, String city, String spot,
-			int day, int month, int year, String agency, String url) {
-		SQLiteDatabase db = getWritableDatabase();
-		ContentValues cv = new ContentValues();
-		cv.put("ARTIST", artistName);
-		cv.put("CITY", city);
-		cv.put("SPOT", spot);
-		cv.put("DAY", day);
-		cv.put("MONTH", month);
-		cv.put("YEAR", year);
-		cv.put("AGENCY", agency);
-		cv.put("URL", url);
-		db.insertOrThrow("Concerts", null, cv);
+	public void addConcert(String artistName,String city,String spot,
+			int day,int month,int year,String agency,String url){
+		if(!contains(artistName,city,spot,day,month,year)){
+			ContentValues cv = new ContentValues();
+			cv.put("ARTIST",artistName);
+			cv.put("CITY", city);
+			cv.put("SPOT", spot);
+			cv.put("DAY",day);
+			cv.put("MONTH", month);
+			cv.put("YEAR",year);	 
+			cv.put("AGENCY", agency);
+			cv.put("URL", url);
+			database.insertOrThrow("Concerts",null,cv);
+		}
+		else
+			System.out.println("Nie dodano "+artistName+city);
+	}
+	
+	public boolean contains(String artistName,String city,String spot,int day,int month,int year){
+		boolean contains = false;
+		int h1 = (artistName+city+spot+String.valueOf(day)+String.valueOf(month)+String.valueOf(year)).hashCode();
+		String[] columns = {"ARTIST","CITY","SPOT","DAY","MONTH","YEAR"};
+		Cursor c = database.query("Concerts",columns,null,null,null,null,null);
+		int h2;
+		while(c.moveToNext()&&!contains){
+			h2 = (c.getString(0)+c.getString(1)+c.getString(2)+String.valueOf(c.getInt(3))+
+					String.valueOf(c.getInt(4))+String.valueOf(c.getInt(5))).hashCode();
+			contains = h1==h2;
+		}
+		c.close();
+		return contains;
 	}
 
 	public Cursor getData() {
 		String[] columns = { "ORD", "ARTIST", "CITY", "SPOT", "DAY", "MONTH", "YEAR", "AGENCY", "URL" };
 		// dodane pobieranie ID na pocz¹tku
-		SQLiteDatabase db = getReadableDatabase();
-		Cursor c = db.query("Concerts", columns, null, null, null, null, null);
+		Cursor c = database.query("Concerts", columns, null, null, null, null, null);
 		return c;
 	}
 
@@ -130,5 +155,53 @@ public class dbManager extends SQLiteOpenHelper {
 			String update = "UPDATE Hashcodes SET HASH = '" + hash + "' WHERE AGENCY = '" + agencyName + "'";
 			db.execSQL(update);
 		}
+	}
+	
+	/*
+	 *  METODY ZASTÊPUJ¥CE CMa:
+	 */
+
+	private String[] universalGetter3000(String columnName){
+		String [] column = {columnName};
+		Cursor c = database.query("Concerts",column,null,null,null,null,null);
+		int size = c.getCount();
+		String[] array = new String[size];
+		for(int i =0; c.moveToNext();i++)
+			array[i] = c.getString(0);
+		c.close();
+		return array;
+	}
+	
+	private String[] deleteDuplicates(String[] arr){
+		HashSet<String> hashSet = new HashSet<String>(Arrays.asList(arr));
+		String[] res = new String[hashSet.size()];
+		hashSet.toArray(res);
+		return res;
+	}
+	
+	public String[] getArtists(){
+		return deleteDuplicates(universalGetter3000("ARTIST"));
+	}
+	
+	public String[] getCities(){
+		return deleteDuplicates(universalGetter3000("CITY"));
+	}
+	
+	
+	public String getArtist(int ID){
+		
+	}
+	
+	public String getCity (int ID){
+		
+	}
+	
+	public String getPlace (int ID){
+		
+	}
+	
+	private String fieldGetter (int ID, String fieldName){
+		String [] columns = {"ORD",fieldName};
+		Cursor c = database.query("Concerts", columns, null,null,null,null,null);
 	}
 }
