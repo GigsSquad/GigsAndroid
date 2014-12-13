81743876f0653e174374f885610bbf9db83286b4
simur407 <szymon.bartczak@op.pl>
2014-12-13 01:12:54 +0100

    dodano przycisk na action bar do slider menu

--- src/pl/javaparty/concertfinder/MainActivity.java
+++ src/pl/javaparty/concertfinder/MainActivity.java
@@ -1,18 +1,19 @@
 package pl.javaparty.concertfinder;
 
-import java.io.IOException;
-import pl.javaparty.jsoup.JSoupDownloader;
 import pl.javaparty.sql.dbManager;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.content.Context;
+import android.content.res.Configuration;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
@@ -30,6 +31,7 @@ public class MainActivity extends Activity {
 	int currentFragment = 1;
 	dbManager dbMgr;
 	Bundle arguments;
+	private ActionBarDrawerToggle drawerToggle;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -43,13 +45,32 @@ public class MainActivity extends Activity {
 		Log.i("DB", "Sprawdzam czy baza istnieje");
 		//TODO tego mialo nie byc
 		new DownloadTask().execute();
-
+		
 		menu = new String[] { "Szukaj", "Ostatnie koncerty", "Twoje koncerty", "Aktualizuj", "Preferencje", "Informacje" };
 		drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 		
 		drawerList = (ListView) findViewById(R.id.left_drawer);
 		adapterDrawer = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, menu);
 		drawerList.setAdapter(adapterDrawer);
+		
+		//ustawianie actionbara by mozna go bylo wcisnac
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+		getActionBar().setHomeButtonEnabled(true);
+		
+		drawerToggle = new ActionBarDrawerToggle(this, drawerLayout,
+				R.drawable.ic_drawer, //nav menu toggle icon
+				R.string.app_name, // nav drawer open - description for accessibility
+				R.string.app_name // nav drawer close - description for accessibility
+		) {
+			public void onDrawerClosed(View view) {
+				invalidateOptionsMenu();
+			}
+
+			public void onDrawerOpened(View drawerView) {
+				invalidateOptionsMenu();
+			}
+		};
+		drawerLayout.setDrawerListener(drawerToggle);
 		drawerList.setSelector(android.R.color.holo_blue_dark);
 		drawerList.setOnItemClickListener(new OnItemClickListener() {
 			@Override
@@ -98,6 +119,29 @@ public class MainActivity extends Activity {
 		MenuInflater inflater = getMenuInflater();
 		inflater.inflate(R.menu.activity_main_actions, menu);
 		return super.onCreateOptionsMenu(menu);
+	}
+	
+	public boolean onOptionsItemSelected(MenuItem item) 
+	{
+		// toggle nav drawer on selecting action bar app icon/title
+		if (drawerToggle.onOptionsItemSelected(item)) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+		// Sync the toggle state after onRestoreInstanceState has occurred.
+		drawerToggle.syncState();
+	}
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		// Pass any configuration change to the drawer toggls
+		drawerToggle.onConfigurationChanged(newConfig);
 	}
 	
 	private class DownloadTask extends AsyncTask<Void, Void, String> {
