aac57bcefd95a66f23e14fa0a73411fc260f5a5e
evelan <evelan3094@gmail.com>
2015-02-01 20:43:00 +0100

    Działający fejsbucz

---
+++ ConcertFinder/src/main/java/pl/javaparty/concertfinder/FacebookFragment.java
@@ -0,0 +1,102 @@
+package pl.javaparty.concertfinder;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+import com.facebook.*;
+import com.facebook.model.GraphUser;
+import com.facebook.widget.LoginButton;
+
+public class FacebookFragment extends Fragment {
+
+	private static final String TAG = "MainFragment";
+	private UiLifecycleHelper uiHelper;
+	private String fbAccessToken;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		uiHelper = new UiLifecycleHelper(getActivity(), callback);
+		uiHelper.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater,
+			ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.activity_splash_screen, container, false);
+
+		LoginButton authButton = (LoginButton) view.findViewById(R.id.authButton);
+		authButton.setFragment(this);
+
+		return view;
+	}
+
+	private void onSessionStateChange(Session session, SessionState state, Exception exception) {
+		if (state.isOpened()) {
+			Log.i(TAG, "Logged in...");
+			fbAccessToken = session.getAccessToken();
+
+			Request.executeMeRequestAsync(session, new Request.GraphUserCallback() {
+				@Override
+				public void onCompleted(GraphUser user, Response response) {
+					Toast.makeText(getActivity(), "Uszanowanko, " + user.getName() + "!", Toast.LENGTH_LONG).show();
+					Intent intent = new Intent(getActivity(), MainActivity.class);
+					startActivity(intent);
+				}
+			});
+			}else if (state.isClosed()) {
+				Log.i(TAG, "Logged out...");
+			}
+		}
+
+		private Session.StatusCallback callback = new Session.StatusCallback() {
+			@Override
+			public void call(Session session, SessionState state, Exception exception) {
+				onSessionStateChange(session, state, exception);
+			}
+		};
+
+		@Override
+		public void onResume () {
+			super.onResume();
+
+			Session session = Session.getActiveSession();
+
+			if (session != null &&
+					(session.isOpened() || session.isClosed())) {
+				onSessionStateChange(session, session.getState(), null);
+			}
+
+			uiHelper.onResume();
+		}
+
+		@Override
+		public void onActivityResult ( int requestCode, int resultCode, Intent data){
+			super.onActivityResult(requestCode, resultCode, data);
+			uiHelper.onActivityResult(requestCode, resultCode, data);
+		}
+
+		@Override
+		public void onPause () {
+			super.onPause();
+			uiHelper.onPause();
+		}
+
+		@Override
+		public void onDestroy () {
+			super.onDestroy();
+			uiHelper.onDestroy();
+		}
+
+		@Override
+		public void onSaveInstanceState (Bundle outState){
+			super.onSaveInstanceState(outState);
+			uiHelper.onSaveInstanceState(outState);
+		}
+	}
--- ConcertFinder/src/main/java/pl/javaparty/concertfinder/SplashScreen.java
+++ ConcertFinder/src/main/java/pl/javaparty/concertfinder/SplashScreen.java
@@ -5,16 +5,34 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.Button;
+import com.google.android.gms.common.ConnectionResult;
 
 public class SplashScreen extends FragmentActivity {
 
 	Button skipBtn;
+	private FacebookFragment facebookFragment;
+	private boolean mSignInClicked;
+	private ConnectionResult mConnectionResult;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_splash_screen);
 
+		if (savedInstanceState == null) {
+			// Add the fragment on initial activity setup
+			facebookFragment = new FacebookFragment();
+			getSupportFragmentManager()
+					.beginTransaction()
+					.add(android.R.id.content, facebookFragment)
+					.commit();
+		} else {
+			// Or set the fragment from restored state info
+			facebookFragment = (FacebookFragment) getSupportFragmentManager()
+					.findFragmentById(android.R.id.content);
+		}
+
+		//findViewById(R.id.sign_in_button).setOnClickListener(SplashScreen.this);
 
 		skipBtn = (Button) findViewById(R.id.skipBtn);
 		skipBtn.setOnClickListener(new View.OnClickListener() {
@@ -24,4 +42,5 @@ public class SplashScreen extends FragmentActivity {
 			}
 		});
 	}
+
 }
