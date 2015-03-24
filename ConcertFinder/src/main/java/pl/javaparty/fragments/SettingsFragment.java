2489a5af17536819d47ffacc00c1778f2252cbfe
evelan <evelan3094@gmail.com>
2015-03-24 13:56:24 +0100

    merge

--- ConcertFinder/src/main/java/pl/javaparty/fragments/SettingsFragment.java
+++ ConcertFinder/src/main/java/pl/javaparty/fragments/SettingsFragment.java
@@ -15,10 +15,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
-import android.widget.Button;
-import android.widget.Toast;
+import android.widget.*;
 import com.facebook.Session;
 import com.facebook.SessionState;
 import com.facebook.UiLifecycleHelper;
@@ -35,90 +32,110 @@ import java.util.Arrays;
 
 public class SettingsFragment extends Fragment {
 
-	AutoCompleteTextView citySearchBox;
-	Button saveButton, clearButton;
-	Context context;
-	ArrayAdapter<CharSequence> adapter;
-	static dbManager dbm;
+    AutoCompleteTextView citySearchBox;
+    Button saveButton, clearButton;
+    RadioButton distanceSort, dateSort;
+    Context context;
+    ArrayAdapter<CharSequence> adapter;
+    static dbManager dbm;
     ProgressDialog mapDialog;
     MapHelper mapHelper;
+    String sortOrder = "YEAR,MONTH,DAY";
 
-	private static final String TAG = "Facebook";
-	private UiLifecycleHelper uiHelper;
-	private Session.StatusCallback callback = new Session.StatusCallback() {
-		@Override
-		public void call(Session session, SessionState state, Exception exception) {
-			onSessionStateChange(session, state, exception);
-		}
-	};
+    private static final String TAG = "Facebook";
+    private UiLifecycleHelper uiHelper;
+    private Session.StatusCallback callback = new Session.StatusCallback() {
+        @Override
+        public void call(Session session, SessionState state, Exception exception) {
+            onSessionStateChange(session, state, exception);
+        }
+    };
 
-	@Override public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		uiHelper = new UiLifecycleHelper(getActivity(), callback);
-		uiHelper.onCreate(savedInstanceState);
-	}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        uiHelper = new UiLifecycleHelper(getActivity(), callback);
+        uiHelper.onCreate(savedInstanceState);
+    }
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle args) {
-		View view = inflater.inflate(R.layout.fragment_settings, container, false);
-		context = inflater.getContext();
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle args) {
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+        context = inflater.getContext();
         getActivity().getActionBar().setTitle(getString(R.string.action_settings));
         dbm = MainActivity.getDBManager();// przekazujemy dbm od mainActivity
         citySearchBox = (AutoCompleteTextView) view.findViewById(R.id.cityAutoComplete);
         saveButton = (Button) view.findViewById(R.id.saveSettingsButton);
-		clearButton = (Button) view.findViewById(R.id.clearFilesButton);
+        clearButton = (Button) view.findViewById(R.id.clearFilesButton);
+        distanceSort = (RadioButton) view.findViewById(R.id.radioButtonDist);
+        dateSort = (RadioButton) view.findViewById(R.id.radioButtonDate);
         mapDialog = new ProgressDialog(getActivity());
         mapDialog.setCancelable(false);
 
-		adapter = ArrayAdapter.createFromResource(getActivity(), R.array.COUNTIES, android.R.layout.simple_dropdown_item_1line);
-		citySearchBox.setAdapter(adapter);
+        adapter = ArrayAdapter.createFromResource(getActivity(), R.array.COUNTIES, android.R.layout.simple_dropdown_item_1line);
+        citySearchBox.setAdapter(adapter);
 
-		citySearchBox.setThreshold(1);
+        citySearchBox.setThreshold(1);
 
-		if (Prefs.getCity(getActivity()) != null)
-			citySearchBox.setText(Prefs.getCity(getActivity()));
+        if (Prefs.getCity(getActivity()) != null)
+            citySearchBox.setText(Prefs.getCity(getActivity()));
 
-		saveButton.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-			Prefs.setCity(getActivity(), citySearchBox.getText().toString());
-				Log.i("SETTINGS", "Zapisano");
-				Log.i("SETTINGS", "Miasto: " + citySearchBox.getText().toString());
+        saveButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Prefs.setCity(getActivity(), citySearchBox.getText().toString());
+                Prefs.setSortOrder(getActivity(), sortOrder);
+                Log.i("SETTINGS", "Zapisano");
+                Log.i("SETTINGS", "Miasto: " + citySearchBox.getText().toString());
                 Toast.makeText(getActivity(), getString(R.string.saved), Toast.LENGTH_SHORT).show();
                 new GetLatLng().execute();
             }
 
-		});
+        });
 
-		clearButton.setOnClickListener(new OnClickListener() {
+        clearButton.setOnClickListener(new OnClickListener() {
 
-			@Override
-			public void onClick(View v) {
-				DialogFragment dialog = new ClearDialog();
-				dialog.show(getActivity().getFragmentManager(), "CLEAR");
-			}
-		});
+            @Override
+            public void onClick(View v) {
+                DialogFragment dialog = new ClearDialog();
+                dialog.show(getActivity().getFragmentManager(), "CLEAR");
+            }
+        });
 
-		LoginButton authButton = (LoginButton) view.findViewById(R.id.authButton);
-		authButton.setFragment(this);
-		authButton.setReadPermissions(Arrays.asList("user_location", "user_birthday", "user_likes"));
+        distanceSort.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sortOrder = "DIST";
+            }
+        });
 
-		return view;
-	}
+        dateSort.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sortOrder = "YEAR,MONTH,DAY";
+            }
+        });
 
-	private void onSessionStateChange(Session session, SessionState state, Exception exception) {
-		if (state.isOpened()) {
-			Log.i(TAG, "Logged in...");
-		} else if (state.isClosed()) {
-			Log.i(TAG, "Logged out...");
-		}
-	}
+        LoginButton authButton = (LoginButton) view.findViewById(R.id.authButton);
+        authButton.setFragment(this);
+        authButton.setReadPermissions(Arrays.asList("user_location", "user_birthday", "user_likes"));
 
-	public static class ClearDialog extends DialogFragment {
+        return view;
+    }
 
-		@Override
-		public Dialog onCreateDialog(Bundle savedInstanceState) {
-			AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+    private void onSessionStateChange(Session session, SessionState state, Exception exception) {
+        if (state.isOpened()) {
+            Log.i(TAG, "Logged in...");
+        } else if (state.isClosed()) {
+            Log.i(TAG, "Logged out...");
+        }
+    }
+
+    public static class ClearDialog extends DialogFragment {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
             builder.setTitle(getString(R.string.question_clear))
                     .setMessage(getString(R.string.msg_clear))
                     .setPositiveButton(getString(R.string.ok), new DialogInterface.OnClickListener() {
@@ -143,46 +160,46 @@ public class SettingsFragment extends Fragment {
                         }
                     });
             return builder.create();
-		}
-	}
+        }
+    }
 
-	@Override
-	public void onResume() {
-		super.onResume();
+    @Override
+    public void onResume() {
+        super.onResume();
 
-		Session session = Session.getActiveSession();
+        Session session = Session.getActiveSession();
 
-		if (session != null &&
-				(session.isOpened() || session.isClosed())) {
-			onSessionStateChange(session, session.getState(), null);
-		}
+        if (session != null &&
+                (session.isOpened() || session.isClosed())) {
+            onSessionStateChange(session, session.getState(), null);
+        }
 
-		uiHelper.onResume();
-	}
+        uiHelper.onResume();
+    }
 
-	@Override
-	public void onActivityResult(int requestCode, int resultCode, Intent data) {
-		super.onActivityResult(requestCode, resultCode, data);
-		uiHelper.onActivityResult(requestCode, resultCode, data);
-	}
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        uiHelper.onActivityResult(requestCode, resultCode, data);
+    }
 
-	@Override
-	public void onPause() {
-		super.onPause();
-		uiHelper.onPause();
-	}
+    @Override
+    public void onPause() {
+        super.onPause();
+        uiHelper.onPause();
+    }
 
-	@Override
-	public void onDestroy() {
-		super.onDestroy();
-		uiHelper.onDestroy();
-	}
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        uiHelper.onDestroy();
+    }
 
-	@Override
-	public void onSaveInstanceState(Bundle outState) {
-		super.onSaveInstanceState(outState);
-		uiHelper.onSaveInstanceState(outState);
-	}
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        uiHelper.onSaveInstanceState(outState);
+    }
 
 
     class GetLatLng extends AsyncTask<String, Void, String> {
--- ConcertFinder/src/main/java/pl/javaparty/prefs/Prefs.java
+++ ConcertFinder/src/main/java/pl/javaparty/prefs/Prefs.java
@@ -54,7 +54,10 @@ public class Prefs {
 
     public static String getSortOrder(Context context) {
             return getPrefs(context).getString("SORTORDER","YEAR,MONTH,DAY");
+    }
 
+    public static void setSortOrder(Context context, String value) { //data -> YEAR,MONTH,DAY, miejsce -> DIST
+        getPrefs(context).edit().putString("SORTORDER", value).commit();
     }
 
 }
