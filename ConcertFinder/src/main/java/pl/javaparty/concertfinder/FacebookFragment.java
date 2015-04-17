8bfe487ea36a0212cf74970c31faf2a80c2ab67b
evelan <evelan3094@gmail.com>
2015-04-18 01:28:46 +0200

    Działający Google+

--- ConcertFinder/src/main/java/pl/javaparty/concertfinder/FacebookFragment.java
+++ ConcertFinder/src/main/java/pl/javaparty/concertfinder/FacebookFragment.java
@@ -1,8 +1,10 @@
 package pl.javaparty.concertfinder;
 
+import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
@@ -18,6 +20,10 @@ import android.widget.Toast;
 import com.facebook.*;
 import com.facebook.model.GraphUser;
 import com.facebook.widget.LoginButton;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.plus.Plus;
+import com.google.android.gms.plus.model.people.Person;
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONArray;
@@ -27,12 +33,22 @@ import pl.javaparty.enums.PHPurls;
 import pl.javaparty.prefs.Prefs;
 import pl.javaparty.sql.JSONthing;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
-public class FacebookFragment extends Fragment {
+public class FacebookFragment extends Fragment implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
 
+    /* GOOGLE PLUS */
+    private static final int RC_SIGN_IN = 0;
+    private GoogleApiClient mGoogleApiClient;
+    private boolean mSignInClicked;
+    private boolean mIntentInProgress;
+
+    /* FACEBOOK */
     LoginButton authButton;
     Button skipButton;
     private UiLifecycleHelper uiHelper;
@@ -43,14 +59,29 @@ public class FacebookFragment extends Fragment {
         }
     };
 
+    final String GOOGLE_FORMAT = "yyyy-mm-dd";
+    final String DB_FORMAT = "dd/mm/yyyy";
+
     ProgressDialog loadingDialog;
     Intent mainActivity;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //FACEBOOK
         uiHelper = new UiLifecycleHelper(getActivity(), callback);
         uiHelper.onCreate(savedInstanceState);
+
+        //GOOGLE PLUS
+        mGoogleApiClient = new GoogleApiClient.Builder(getActivity().getApplicationContext())
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Plus.API)
+                .addScope(Plus.SCOPE_PLUS_LOGIN)
+                .build();
+
+
     }
 
     @Override
@@ -75,9 +106,13 @@ public class FacebookFragment extends Fragment {
             }
         });
 
+        //FACEBOOK
         authButton = (LoginButton) view.findViewById(R.id.authButton);
         authButton.setFragment(this);
         authButton.setReadPermissions(Arrays.asList("user_location", "user_birthday", "user_likes", "email"));
+
+        //GOOGLE PLUS
+        view.findViewById(R.id.sign_in_button).setOnClickListener(this);
 
         return view;
     }
@@ -88,9 +123,10 @@ public class FacebookFragment extends Fragment {
         return netInfo != null && netInfo.isConnectedOrConnecting();
     }
 
+    /* FACEBOOK */
     private void onSessionStateChange(Session session, SessionState state, Exception exception) {
         if (state.isOpened()) {
-            Log.i("FB", "Logged in...");
+            Log.i("LOGIN", "Zalogowano przez Facebooka");
 
             // Request user data and show the results
             Request.executeMeRequestAsync(session, new Request.GraphUserCallback() {
@@ -115,13 +151,14 @@ public class FacebookFragment extends Fragment {
 
                         Prefs.setCity(getActivity(), columns[4]);
                         Toast.makeText(getActivity(), getString(R.string.hello) + ", " + columns[0], Toast.LENGTH_SHORT).show();
-                        new loginUser(columns).execute();
+                        if (isAdded())
+                            new loginUser(columns).execute();
                     }
                 }
             });
 
         } else if (state.isClosed()) {
-            Log.i("FB", "Logged out...");
+            Log.i("LOGIN", "Wylogowano z Facebooka");
         }
     }
 
@@ -147,7 +184,8 @@ public class FacebookFragment extends Fragment {
         if (session != null && (session.isOpened() || session.isClosed())) {
             onSessionStateChange(session, session.getState(), null);
         }
-        skipButton.setVisibility(View.VISIBLE);
+        //DEBUG
+        //skipButton.setVisibility(View.VISIBLE);
 
         uiHelper.onResume();
     }
@@ -155,7 +193,20 @@ public class FacebookFragment extends Fragment {
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        uiHelper.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode != Activity.RESULT_OK) {
+                mSignInClicked = false;
+            }
+
+            mIntentInProgress = false;
+
+            if (!mGoogleApiClient.isConnected()) {
+                mGoogleApiClient.reconnect();
+            }
+        } else {
+            uiHelper.onActivityResult(requestCode, resultCode, data);
+        }
     }
 
     @Override
@@ -176,6 +227,119 @@ public class FacebookFragment extends Fragment {
         uiHelper.onSaveInstanceState(outState);
     }
 
+
+    /* GOOGLE PLUS */
+    public void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    public void onStop() {
+        super.onStop();
+
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        if (!mIntentInProgress) {
+            if (mSignInClicked && result.hasResolution()) {
+                // The user has already clicked 'sign-in' so we attempt to resolve all
+                // errors until the user is signed in, or they cancel.
+                try {
+                    result.startResolutionForResult(getActivity(), RC_SIGN_IN);
+                    mIntentInProgress = true;
+                } catch (IntentSender.SendIntentException e) {
+                    // The intent was canceled before it was sent.  Return to the default
+                    // state and attempt to connect to get an updated ConnectionResult.
+                    mIntentInProgress = false;
+                    mGoogleApiClient.connect();
+                }
+            }
+        }
+    }
+
+    public void onClick(View view) {
+        if (view.getId() == R.id.sign_in_button && !mGoogleApiClient.isConnecting()) {
+            mSignInClicked = true;
+            mGoogleApiClient.connect();
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        mSignInClicked = false;
+        Log.i("LOGIN", "Zalogowano przez Google+");
+
+
+        if (Plus.PeopleApi.getCurrentPerson(mGoogleApiClient) != null) {
+            Person currentPerson = Plus.PeopleApi.getCurrentPerson(mGoogleApiClient);
+            String email = Plus.AccountApi.getAccountName(mGoogleApiClient);
+            //nie usuwajcie mi tego
+//            if (
+//                currentPerson.getRelationshipStatus() == 1 &&
+//                currentPerson.getAgeRange().getMax() == 26 &&
+//                currentPerson.getAgeRange().getMin() == 18 &&
+//                currentPerson.getGender() == 0 &&
+//                !currentPerson.getCurrentLocation().equals("Kraków") &&
+//                currentPerson.hasImage())
+//            {
+//                //TODO send email to Jakub
+//
+//            }
+
+
+            String[] columns = new String[10];
+            columns[0] = currentPerson.getDisplayName().split(" ")[0].trim();
+            columns[1] = currentPerson.getDisplayName().split(" ")[1].trim();
+            columns[2] = email.trim();
+
+
+            columns[5] = "-1";
+
+            try {
+                columns[3] = currentPerson.getBirthday().trim();
+                columns[4] = currentPerson.getPlacesLived().get(0).getValue();
+            } catch (NullPointerException e) {
+                e.printStackTrace();
+                columns[3] = "";
+                columns[4] = "";
+            }
+
+
+            SimpleDateFormat sdf = new SimpleDateFormat(GOOGLE_FORMAT);
+            Date d = null;
+            try {
+                d = sdf.parse(columns[3]);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            sdf.applyPattern(DB_FORMAT);
+            columns[3] = sdf.format(d);
+
+            Log.i("LOGIN", "Imie: " + columns[0]);
+            Log.i("LOGIN", "Nazwisko: " + columns[1]);
+            Log.i("LOGIN", "Email: " + columns[2]);
+            Log.i("LOGIN", "Urodziny: " + columns[3]);
+            Log.i("LOGIN", "Lokalizacja: " + columns[4]);
+            Log.i("LOGIN", "Id: " + columns[5]);
+
+
+            Prefs.setCity(getActivity(), columns[4]);
+            Toast.makeText(getActivity(), getString(R.string.hello) + ", " + columns[0], Toast.LENGTH_SHORT).show();
+            if (isAdded())
+                new loginUser(columns).execute();
+        }
+    }
+
+
+    public void onConnectionSuspended(int cause) {
+        mGoogleApiClient.connect();
+    }
+
+    /* BAZA DANYCH PHPY */
     class loginUser extends AsyncTask<String, Void, String> {
 
         String[] array = new String[10];
@@ -196,6 +360,14 @@ public class FacebookFragment extends Fragment {
         @Override
         protected String doInBackground(String... args) {
             List<NameValuePair> params = new ArrayList<>();
+
+            Log.i("LOGIN", "Imie:" + array[0]);
+            Log.i("LOGIN", "Nazwisko: " + array[1]);
+            Log.i("LOGIN", "Email: " + array[2]);
+            Log.i("LOGIN", "Urodziny: " + array[3]);
+            Log.i("LOGIN", "Lokalizacja: " + array[4]);
+            Log.i("LOGIN", "Id: " + array[5]);
+
             //wysyłamy dane użytkownika, jeśli nie ma go w bazie to doda
             params.add(new BasicNameValuePair("firstName", array[0]));
             params.add(new BasicNameValuePair("lastName", array[1]));
@@ -228,4 +400,6 @@ public class FacebookFragment extends Fragment {
             super.onPostExecute(s);
         }
     }
+
+
 }
